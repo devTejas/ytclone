60906fdcd1da7500d190fbf5ef26b0118d9ee08d
Tejas <>
2021-10-16 23:58:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import youtube from "./api/youtube";
+import { SearchBar, VideoDetail, VideoList } from "./components";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
+  return (
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        q: searchTerm,
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY,
+      },
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
+};
+
+export default App;
